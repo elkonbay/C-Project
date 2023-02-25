49540f52be5cc39ae31b4c2e8f5f38398c3f8c03
Elmur Konushbaev <el.konushbaev@gmail.com>
2023-02-25 13:10:38 +0800

    Example 010 Methode

---
+++ Example010_Methode/Program.cs
@@ -0,0 +1,16 @@
+﻿int[] array = {11,29,36,43,51,62,73,58,39};
+
+int n = array.Length;
+int find = 51;
+
+int index = 0;
+
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
