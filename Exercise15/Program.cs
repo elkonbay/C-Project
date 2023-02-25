479883c86a65439ff3c15d7b41e7a865a2b60bf3
Elmur Konushbaev <el.konushbaev@gmail.com>
2023-02-25 13:43:53 +0800

    Exercise 15

---
+++ Exercise15/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите цифру, обозначающую день недели: ");
+int day = Convert.ToInt32(Console.ReadLine());
+
+void Week(int day) {
+  if (day == 6 || day == 7) {
+  Console.WriteLine("да");
+  }
+  else if (day < 1 || day > 7) {
+    Console.WriteLine("Ошибка");
+  }
+  else Console.WriteLine("нет");
+}
+
+Week(day);
